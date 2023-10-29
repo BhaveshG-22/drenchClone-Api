def828e0b300d28045f8701f78c8a863f9af0cb9
BhaveshG-22 <107833962+BhaveshG-22@users.noreply.github.com>
2023-10-29 09:03:07 -0400

    cleaned handelMoves function code

--- helpers/handelMoves.js
+++ helpers/handelMoves.js
@@ -1,37 +1,4 @@
 function HandelMoves(grid, color) {
-  //first chnage color of source to :color and chnage neighbours
-  //color if theya re same as source
-
-  grid[0][0] = color;
-
-  changeColor(0, 0, color, grid);
-
-  return grid;
-}
-
-function changeColor(r, c, color, grid) {
-  if (r < 0 || c < 0 || r > grid.length || c > grid[0].length) {
-    return;
-  }
-
-  grid[r][c] = color;
-
-  //top
-  changeColor(r - 1, c, grid, color);
-
-  //bottom
-  changeColor(r + 1, c, grid, color);
-
-  //right
-  changeColor(r, c + 1, grid, color);
-
-  //left
-  changeColor(r, c - 1, grid, color);
-}
-
-module.exports = HandelMoves;
-
-function HandelMoves(grid, color) {
   const sourceColor = grid[0][0];
   if (sourceColor === color) {
     return grid; // If the source color is the same as the target color, no need to update
